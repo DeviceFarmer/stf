6c6601f91bb3d76cfbe7f3091658783b363a8cb7
Matan Baruch <36934912+matanbaruch@users.noreply.github.com>
2026-09-25 20:27:59 +0300

    fix: resolve waitForEnd right away when the risky stream already ended
    Signed-off-by: Matan Baruch <36934912+matanbaruch@users.noreply.github.com>

---
+++ test/util/riskystream.js
@@ -0,0 +1,38 @@
+var stream = require('stream')
+
+var chai = require('chai')
+var expect = chai.expect
+
+var RiskyStream = require('../../lib/util/riskystream')
+
+describe('RiskyStream', function() {
+  it('should resolve waitForEnd when the stream has already ended', function(done) {
+    var source = new stream.PassThrough()
+    var risky = new RiskyStream(source)
+
+    risky.on('end', function() {
+      risky.waitForEnd()
+        .timeout(100)
+        .then(function(ended) {
+          expect(ended).to.equal(true)
+          done()
+        })
+        .catch(done)
+    })
+
+    source.end()
+    source.resume()
+  })
+
+  it('should resolve waitForEnd once the stream ends', function() {
+    var source = new stream.PassThrough()
+    var risky = new RiskyStream(source)
+    var waiting = risky.waitForEnd()
+
+    source.end()
+
+    return waiting.then(function(ended) {
+      expect(ended).to.equal(true)
+    })
+  })
+})
